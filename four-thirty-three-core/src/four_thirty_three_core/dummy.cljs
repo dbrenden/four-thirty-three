0d284e674c3268fd527f9e487617c2d21e0f5790
Brenden Doyle <brenden.doyle@gmail.com>
2017-04-03 23:19:02 -0300

    dummy namespace init/stop works

--- four-thirty-three-core/src/four_thirty_three_core/dummy.cljs
+++ four-thirty-three-core/src/four_thirty_three_core/dummy.cljs
@@ -28,55 +28,40 @@
   (post [_ params]
     (println "POSTING TO TVVITTER")))
 
-(defn sleep-fn []
-  (let [start-time (atom (tc/to-long (t/now)))
-        post-sleep-length (atom 0)]
-    (fn [scheduled-recorder]
-      (go
-        (let [new-start-time (tc/to-long (t/now))
-              {:keys [period recording-length buffer-length]} scheduled-recorder]
-          (reset! post-sleep-length (- period
-                                       (- new-start-time @start-time)))
-          (reset! start-time new-start-time)
-          (println "POST-RECORD-SLEEP: " {:post-sleep-length @post-sleep-length})
-          (let [pre-sleep-length (rand-int (- period recording-length buffer-length))
-                timeout-length (+ @post-sleep-length pre-sleep-length)]
-            (println "PRE-RECORD-SLEEP: " {:pre-sleep-length pre-sleep-length})
-            (println "TIMEOUT LENGTH: " {:timeout-length timeout-length})
-            (a/<! (a/timeout timeout-length))))))))
-
-(defrecord ScheduledRecorder [recorder period recording-length buffer-length sleep-fn output> stop>]
+(defrecord ScheduledRecorder [recorder period recording-length buffer-length output> stop>]
   sp/Scheduled
   (init [this]
     (go
-      (loop []
-        (if-not (a/poll! stop>)
-          (do (<! (sp/perform this))
-              (alts! stop> (sp/sleep this))
-              (recur))
-          (do (println "Shutting down recording")
-              (a/close! output>))))))
+      (loop [start-time (tc/to-long (t/now))
+             pre-sleep-length 0]
+        (let [_ (<! (sp/perform this))
+              post-record-time (tc/to-long (t/now))
+              post-sleep-length (- period pre-sleep-length
+                                   (- post-record-time start-time))
+              pre-sleep-length (rand-int (- period recording-length buffer-length))
+              timeout-length (+ post-sleep-length pre-sleep-length)]
+          (println "POST-RECORD-SLEEP: " {:post-sleep-length post-sleep-length})
+          (println "PRE-RECORD-SLEEP: " {:pre-sleep-length pre-sleep-length})
+          (println "TIMEOUT LENGTH: " {:timeout-length timeout-length})
+          (alts! [stop> (a/timeout timeout-length)])
+          (if-not (a/poll! stop>)
+            (recur (tc/to-long (t/now)) pre-sleep-length)
+            (do (println "Shutting down recording")
+                (a/close! output>)))))))
   (perform [this]
-    (go (let [recording (rp/start recorder {})]
-          (a/<! (a/timeout recording-length))
-          (a/>! output> (rp/stop recorder recording)))))
-  (sleep [this] (sleep-fn))
+    (go (let [recording (rp/start recorder {})
+              [v c] (alts! [stop> (a/timeout recording-length)])]
+          (when-not (= c stop>)
+            (a/>! output> (rp/stop recorder recording))))))
   (stop [this]
     (go (a/>! stop> true))))
-
-(defonce stop-fn (atom nil))
-
-(defn stop []
-  (when-let [stop-fn @stop-fn]
-    (stop-fn)))
 
 (defn instantiate []
   (let [scheduled-recorder (map->ScheduledRecorder. {:recorder (Recorder.)
                                                      :period 60000
                                                      :recording-length 5000
                                                      :buffer-length 10000
-                                                     :sleep-fn (sleep-fn)
                                                      :output> (a/chan 100)
                                                      :stop> (a/promise-chan)})]
     (sp/init scheduled-recorder)
-    (reset! stop-fn (fn [] (sp/stop scheduled-recorder)))))
+    scheduled-recorder))
